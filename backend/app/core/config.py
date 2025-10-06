b9f92259cdb83b29e0313c2d4f702f705dd68bae
Nho89 <noa1720@gmail.com>
2025-10-06 11:17:56 +0200

    feat: added variables openrouter and supabase

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,6 +9,7 @@ class Settings:
     VERSION: str = "0.1.0"
     DEBUG: bool = True
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
+    OPENROUTER_API_KEY: str = os.getenv("OPENROUTER_API_KEY", "") 
     #supabase :
     SUPABASE_URL: str = os.getenv("SUPABASE_URL", "")
     SUPABASE_KEY: str = os.getenv("SUPABASE_KEY", "")
@@ -17,5 +18,13 @@ class Settings:
     DATASET_PATH: str = os.getenv("DATASET_PATH", "data/migration_plan.csv")
     # origins para CORS
     ORIGINS = os.getenv("ALLOWED_ORIGINS", "").split(",")
+    #supabase para subir los informes:
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY") 
+    # Bucket para los XML originales
+    SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET", "nifi-docs")
+    # Bucket para los informes de migración
+    SUPABASE_BUCKET1 = os.getenv("SUPABASE_BUCKET1", "history")
+
 
 settings = Settings()
--- backend/app/core/llms.py
+++ backend/app/core/llms.py
@@ -1,9 +1,16 @@
 from .config import settings
 from langchain_groq import ChatGroq
-
+from langchain_openai import ChatOpenAI
 # En este archivo definimos la configuración del LLM que usaremos en los agentes
 
 llm = ChatGroq(
     api_key=settings.GROQ_API_KEY,
     model="groq/meta-llama/llama-4-scout-17b-16e-instruct"
+)
+
+llm_validator = ChatOpenAI(
+    api_key=settings.OPENROUTER_API_KEY,
+    model="openrouter/meta-llama/llama-3.1-70b-instruct",
+    base_url="https://openrouter.ai/api/v1",
+    model_kwargs={"extra_headers": {"HTTP-Referer": "nifi-migrator-ai"}}
 )

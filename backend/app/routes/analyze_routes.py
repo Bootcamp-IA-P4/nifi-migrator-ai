8642b89e68f44b9699c719d2c0a0b3e263d1189a
Nho89 <noa1720@gmail.com>
2025-10-06 11:19:13 +0200

    feat: added routes save report in markdown

--- backend/app/routes/analyze_routes.py
+++ backend/app/routes/analyze_routes.py
@@ -1,8 +1,9 @@
 # Endpoints para anÃ¡lisis de XML
-from fastapi import APIRouter, UploadFile, Response
-from app.services import analyzer
+from fastapi import APIRouter, UploadFile, Response, Form, HTTPException
+from app.services import analyzer, pdf_generator, supabase_registry
 from app.models.report import Report
-import json
+from app.core.config import settings
+import os
 
 router = APIRouter()
 
@@ -11,3 +12,37 @@ async def analyze_xml(file: UploadFile):
     content = await file.read()
     result = analyzer.analyze_nifi_xml(content)
     return result
+
+@router.post("/pdf", summary="Analiza un XML y devuelve un informe en PDF")
+async def analyze_and_get_pdf(
+    file: UploadFile,
+    store_report: bool = Form(False)
+):
+    if not file.filename or not file.filename.endswith('.xml'):
+        raise HTTPException(status_code=400, detail="El archivo debe ser un .xml")
+
+    xml_content = await file.read()
+    
+    report_result = analyzer.analyze_nifi_xml(xml_content)
+
+    if report_result.error:
+        raise HTTPException(status_code=500, detail=report_result.error)
+
+    pdf_bytes = pdf_generator.create_pdf_from_markdown(report_result.raw_markdown)
+
+    if store_report:
+        markdown_filename = os.path.splitext(file.filename)[0] + ".md"
+        
+        markdown_bytes = report_result.raw_markdown.encode('utf-8')
+        
+        supabase_registry.upload_file_to_bucket(
+            file_name=markdown_filename,
+            file_bytes=markdown_bytes,
+            bucket=settings.SUPABASE_BUCKET1 
+        )
+    pdf_bytes = pdf_generator.create_pdf_from_markdown(report_result.raw_markdown)
+    pdf_download_name = os.path.splitext(file.filename)[0] + "_migration_report.pdf"
+    headers = {
+        'Content-Disposition': f'attachment; filename="{pdf_download_name}"'
+    }
+    return Response(content=pdf_bytes, media_type="application/pdf", headers=headers)

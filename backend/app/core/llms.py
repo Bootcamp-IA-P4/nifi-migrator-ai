69375e13f8cf8923d5de2768d7afaba795afa2b4
Nho89 <noa1720@gmail.com>
2025-10-20 01:25:34 +0200

    chore: change to anthropic model

--- backend/app/core/llms.py
+++ backend/app/core/llms.py
@@ -13,7 +13,7 @@ llm_validator = ChatOpenAI(
 
 
 llm = ChatLiteLLM(
-    model="openrouter/mistralai/mistral-7b-instruct:free",   # este es el de pago, solo se cambiaría , pero usar solamente cuando sea necesario: "anthropic/claude-sonnet-4-20250514"
+    model="anthropic/claude-sonnet-4-20250514",   # este es el de pago, solo se cambiaría , pero usar solamente cuando sea necesario: "anthropic/claude-sonnet-4-20250514"
     temperature=0.1,
     max_output_tokens=8192,
     litellm_params={
